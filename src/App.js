0ad442db18e1e92be10624f52829aa30d0bf8d91
Isaac Lyons <isaac@snowskeleton.net>
2022-04-20 01:50:58 -0400

    I hate everything about this, but I think I finally have full access to my objects

--- src/App.js
+++ src/App.js
@@ -7,29 +7,12 @@ function App() {
   return (
     <div className="App">
 
-        {/* <Main /> */}
         <Routes>
           <Route path="/getPoll/:pollID" element={<Poll />}></Route>
           <Route path='/' element={<MakePoll />}></Route>
-            {/* <Route path=':pollID' element={<Poll />}></Route> */}
-          {/* </Route> */}
         </Routes>
-        {/* { show?<MakePoll></MakePoll>:<Routes> <Route params={poll} element={<PreVote />} /> </Routes> }
-        <Routes> <Route path="getPoll/:pollID" element={<Vote />} /> </Routes> */}
     </div>
   );
 }
-
-
-// function Main() {
-//   return (
-//     <Routes>
-//       <Route path='/getPoll/:pollID' element={Poll}></Route>
-//       {/* <Route path='/about' component={About}></Route>
-//       <Route path='/contact' component={Contact}></Route> */}
-//       <Route path='/' element={MakePoll}></Route>
-//     </Routes>
-//   );
-// }
 
 export default App;
--- src/Poll.js
+++ src/Poll.js
@@ -1,24 +1,70 @@
 import { useParams } from "react-router-dom"
 import React, { useState, useEffect } from 'react';
-// import ProgressBar from './Progress_bar'
+import ProgressBar from './Progress_bar'
+import { type } from "@testing-library/user-event/dist/type";
 
+function UnPoll(wood) {
+    this.id = Object.entries(wood)[0][1]['id']
+    this.name = Object.entries(wood)[0][1]['name']
+    this.options = Object.entries( Object.entries(wood)[0][1])[0][1]
+    this.options = Object.fromEntries( Object.entries(this.options))
+    this.options.fir = Object.entries(this.options)[0][0]
+    this.options.sec = Object.entries(this.options)[0][1]
+
+    console.log (
+        JSON.stringify (
+            console.log(Object.entries(this.options)[0][1]['id'])
+                // Object.entries(
+                // )
+        )
+    )
+
+
+    // console.log(this.options)
+    // console.log(Object.entries(Object.entries(wood)[0][1])[0][1][1]['id'])
+    // this.options.id = Object.entries( Object.entries(wood)[0][1])[0][1][1]['id']
+    // this.options.vote = Object.entries( Object.entries(wood)[0][1])[0][1][1]['vote']
+    // this.options.text = Object.entries( Object.entries(wood)[0][1])[0][1][1]['text']
+    // this.options = Object.entries(wood)[0]['options']['1']
+    // this.options.id = Object.entries(wood)[0][1]['options']['id']
+    // this.options.votes = Object.entries(wood)[0][1]['options']['votes']
+    // this.options.text = Object.entries(wood)[0][1]['options']['text']
+}
 export default function Poll() {
     let params = useParams()
-    const [dat, setPoll] = useState(" not yet an object ")
+    const [poll, setPoll] = useState("WRONG")
 
     useEffect(() => {
         fetch("https://df7h7a2mb9.execute-api.us-east-2.amazonaws.com/getPoll/" + params.pollID)
             .then(response => response.json())
-            .then(data => { setPoll(data)})
+            .then(data => {
+                const v = Object.entries(data)
+                const a = Object.fromEntries(v)
+                setPoll(a)
+            })
+
     }, []);
 
 
     return (
         <div className="Poll">
-            <h2>Poll:  {JSON.stringify(dat.Item)} </h2>
-            {params.pollID}
-            {/* <ProgressBar bgcolor='green' progress={poll.options[0]} height={30} width={10} />
-        <ProgressBar bgcolor='green' progress={poll.options[1]} height={30} width={10} /> */}
+            <h2> {
+                // console.log(
+                //     JSON.stringify(
+                //         Object.entries(
+                //             Object.entries(poll)[0][1]
+                //         )[0][1][1]
+
+                //     )
+                // )
+            }</h2>
+            <h2> {new UnPoll(poll).id} </h2>
+            <h2> {new UnPoll(poll).name} </h2>
+            <h2> {JSON.stringify(new UnPoll(poll).options)} </h2>
+            {/* <h2> {new JSON.stringify(UnPoll(poll).options)} </h2> */}
+            {/* {params.pollID} */}
+            {/* <ProgressBar bgcolor='green' progress={params.options[0]} height={30} width={10} /> */}
+            {/* <ProgressBar bgcolor='green' progress={poll} height={30} width={10} /> */}
 
             {/* <button onClick={useEffect}>
                 Create
